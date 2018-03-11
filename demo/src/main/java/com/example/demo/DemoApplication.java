9b812bb54ccefe6e7814f20d20b95609b11ef4cf
rukesh <rukeshtechnology@gmail.com>
2018-03-11 15:59:39 -0400

    ws2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("yahoo from git 2...");
+		System.out.println("yahoo from git 2...lll");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
