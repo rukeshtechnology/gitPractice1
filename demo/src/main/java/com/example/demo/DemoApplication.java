6661f09a949f83fffaba217e98c7a3ea46104f6b
rukesh <rukeshtechnology@gmail.com>
2018-03-11 15:32:12 -0400

    From gitone

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("from gittwo");
+		System.out.println("from gitone");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
