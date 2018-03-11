9e7ca22867916dde9ae353ed9c77d18fcd20a346
rukesh <rukeshtechnology@gmail.com>
2018-03-11 15:59:01 -0400

    f

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("from gitone again...");
-=======
-		System.out.println("yahoo from git 2...");
->>>>>>> branch 'master' of https://github.com/rukeshtechnology/gitPractice1.git
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
