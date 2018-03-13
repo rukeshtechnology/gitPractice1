1834efb4db666643ba92bfae964d8888b9932be7
rukesh <rukeshtechnology@gmail.com>
2018-03-13 00:22:13 -0400

    3 file conflict.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {		
-		System.out.println("Keeping all branch");
-		System.out.println("Keeping all master");
-		System.out.println("stash2");
-		System.out.println("For stashing");
-		System.out.println("For stashing");
+	public static void main(String[] args) {
+		System.out.println("conflict1");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- demo/src/main/java/com/example/demo/HelloWorld.java
+++ demo/src/main/java/com/example/demo/HelloWorld.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World branch");
-		System.out.println("Hello World master");
+		System.out.println("conflict 1");
 	}
 }
