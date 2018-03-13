85100865586322bff9a978daed70f9c3c39d709b
rukesh <rukeshtechnology@gmail.com>
2018-03-13 00:13:35 -0400

    stash2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {		
 		System.out.println("Keeping all branch");
 		System.out.println("Keeping all master");
+		System.out.println("stash2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
