3c68c7647a7942036da958db69790eeb1df370fe
rukesh <rukeshtechnology@gmail.com>
2018-03-13 00:25:06 -0400

    conflict branch.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("conflict1");
+		System.out.println("conflict branch");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- demo/src/main/java/com/example/demo/HelloUniverse.java
+++ demo/src/main/java/com/example/demo/HelloUniverse.java
@@ -2,5 +2,8 @@ package com.example.demo;
 
 public class HelloUniverse {
 
+	public static void main(String[] args) {
+		System.out.println("Conflict Branch");
+	}
 	
 }
